a8440ad75abca77f67e735ffaa5dae3e23bb6a70
Srimayi Golajapu <srimayiofficial@gmail.com>
2026-02-16 19:31:10 +0530

    Add files via upload

---
+++ result.py
@@ -0,0 +1,43 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:\Users\Acer\AppData\Local\Programs\Python\Python313\grade_calculator.py
+==================================================
+      STUDENT GRADE CALCULATOR
+==================================================
+
+Enter number of students: 2
+
+=== STUDENT 1 ===
+Student name: Alice
+Enter marks (0-100):
+Math: 65
+Science: 70
+English: 85
+
+=== STUDENT 2 ===
+Student name: Harry
+Enter marks (0-100):
+Math: 90
+Science: 69
+English: 54
+
+==================================================
+            RESULTS SUMMARY
+==================================================
+Name                 |   Avg | Grade | Comment
+------------------------------------------------------------
+Alice                |  73.3 |   C   | Good.
+Harry                |  71.0 |   C   | Good.
+
+==================================================
+          CLASS STATISTICS
+==================================================
+Total Students: 2
+Class Average: 72.2
+Highest Average: 73.3 (Alice)
+Lowest Average: 71.0 (Harry)
+
+==================================================
+Thank you for using the Grade Calculator!
+==================================================
